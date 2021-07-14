82d1648dce49502c16f0ef47a6d3135cfec0de49
Hoshimxon <ashuraliyevh.1994@gmail.com>
2021-07-14 15:31:01 +0500

    created SendNotification command

---
+++ app/Console/Commands/SendNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\EmailNotification;
+use App\Jobs\PhoneNotification;
+use App\Models\Product;
+use App\Models\User;
+use App\Notifications\ProductBoughtMailNotification;
+use App\Sms;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:notification {type} {client} {product}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send notification to client about shopping. This command has 3 parameters. First parameter is type which channel type. Second parameter is client id and third is product id';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return int
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     */
+    public function handle()
+    {
+        $client = User::query()->where('id', $this->argument('client'))->first();
+        if (!$client) {
+            $this->info('Client not found!');
+            return 0;
+        }
+
+        $product = Product::query()->where('id', $this->argument('product'))->first();
+        if (!$product) {
+            $this->info('Product not found!');
+            return 0;
+        }
+
+        switch ($this->argument('type')) {
+            case 'mail':
+                EmailNotification::dispatch($client, $product);
+                $this->info('Successfully sent!');
+                break;
+            case 'sms':
+                $message = Sms::sendSmsNotification($client, $product);
+                $this->info($message);
+                break;
+            default:
+                $this->info('Type is incorrect!');
+        }
+    }
+}
